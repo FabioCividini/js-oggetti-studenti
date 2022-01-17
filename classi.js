daa1e860179cb0bc28e96d3fad0f1d106c2449d6
FabioCividini <82501787+FabioCividini@users.noreply.github.com>
2022-01-17 15:14:52 +0100

    creazione di un array di oggetti studenti

--- classi.js
+++ classi.js
@@ -1,10 +1,27 @@
 let studente = {
-    nome: 'Fabio',
-    cognome: 'Cividini',
-    età: '24',
+    nome: 'nome1',
+    cognome: 'cognome1',
+    età: 'età1',
 }
 
 for (let chiave in studente) {
     console.log(chiave);
 }
 
+let studenti = [
+    {
+        nome: 'nome2',
+        cognome: 'cognome2',
+        età: 'etè2',
+    },
+    {
+        nome: 'nome3',
+        cognome: 'cognome3',
+        età: 'etè3',
+    },
+    {
+        nome: 'nome4',
+        cognome: 'cognome4',
+        età: 'etè4',
+    }
+]
